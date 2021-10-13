17e51cc7660a9a9eae4b8df74f3cbfdab391dba3
陈树洪 <shuhong.chen@m2micro.com>
2021-10-13 16:40:58 +0800

    springboot 限流

---
+++ src/main/java/com/chenshuhong/blog/orika/Teacher.java
@@ -0,0 +1,18 @@
+package com.chenshuhong.blog.orika;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author jam
+ * @date 2021/9/2 2:51 下午
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher {
+    private String id;
+    private String name;
+    private String emailAddress;
+}
---
+++ src/main/java/com/chenshuhong/blog/swagger/SwaggerController.java
@@ -0,0 +1,29 @@
+package com.chenshuhong.blog.swagger;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * swagger分组示例
+ * @author jam
+ * @date 2021/7/31 9:10 下午
+ */
+@RestController
+@Slf4j
+@RequestMapping("/swagger")
+@Api(tags = "Swagger接口文档")
+public class SwaggerController {
+
+    @ApiOperation(value = "say",response = String.class)
+    @GetMapping("/say")
+    public String say(){
+        return "Hello,你好";
+    }
+
+
+
+}
